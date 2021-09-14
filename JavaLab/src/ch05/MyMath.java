2190f002cba4107335b884e9f1dccdff72858324
KilyongJeong <uzoolove@gmail.com>
2021-09-14 11:37:41 +0900

    .

---
+++ JavaLab/src/ch05/MyMath.java
@@ -0,0 +1,26 @@
+package ch05;
+
+public class MyMath {
+	
+	// 10 + 20의 결과를 출력한다.
+	void printSum() {
+		
+	}	
+	// 10 + n2의 결과를 출력한다.
+	void printSum(int n2) {
+		
+	}	
+	// n1 + n2의 결과를 출력한다.
+	void printSum(int n1, int n2) {
+		
+	}	
+	// n1 + n2의 결과를 반환한다.
+	void getSum(int n1, int n2) {
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
