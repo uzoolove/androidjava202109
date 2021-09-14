3d9dcfbc43768da565eb5897f9549faf1a4031d6
KilyongJeong <uzoolove@gmail.com>
2021-09-14 13:31:34 +0900

    .

--- JavaLab/src/ch05/MyMath.java
+++ JavaLab/src/ch05/MyMath.java
@@ -2,6 +2,12 @@ package ch05;
 
 public class MyMath {
 	
+	/*
+	 * 메소드 오버로딩
+	 * 	- 같은 클래스 내에서 동일한 이름의 메소드를 중복 정의
+	 * 	- 단, 매개변수의 타입, 개수, 순서를 다르게 정의해야한다.
+	 */
+	
 	// 10 + 20의 결과를 출력한다.
 	static void printSum() {
 		int n1 = 10;
@@ -19,7 +25,7 @@ public class MyMath {
 	void printSum(int n1, int n2) {
 		int result = n1 + n2;
 		System.out.println(n1 + " + " + n2 + " = " + result);
-	}	
+	}
 	// n1 + n2의 결과를 반환한다.
 	static String getSum(int n1, int n2) {
 		int result = n1 + n2;
--- JavaLab/src/ch05/MyMath2.java
+++ JavaLab/src/ch05/MyMath2.java
@@ -4,6 +4,29 @@ public class MyMath2 {
 	int n1;
 	int n2;
 	
+	/*
+	 * 생성자: 객체가 생성될 때 자동으로 한번 호출되는 메소드
+	 * 주로 객체의 인스턴스 변수를 초기화하는 작업을 한다.
+	 * 만드는 방법
+	 * 	- 리턴타입을 기술하지 않는다.
+	 * 	- 클래스명과 동일한 이름으로 만든다.
+	 */
+	MyMath2(int n1, int n2){
+		this.n1 = n1;
+		this.n2 = n2;
+	}
+	
+	/*
+	 * 기본 생성자: 매개변수가 없는 생성자
+	 * 	- 생성자를 하나도 정의하지 않은 클래스는 컴퍼일러에 의해서 자동으로 만들어진다.
+	 * 	- 생성자를 명시적으로 하나라도 정의할 경우 자동으로 만들지 않는다.
+	 */
+	MyMath2(){
+//		this.n1 = 20;
+//		this.n2 = 10;
+		this(10, 20);	// 자신의 생성자를 호출한다.(생성자 내부에서만 가능)
+	}
+	
 	void plus() {
 		System.out.println(n1 + " + " + n2 + " = " + (n1+n2));
 	}
@@ -19,8 +42,10 @@ public class MyMath2 {
 
 	public static void main(String[] args) {
 		MyMath2 m2 = new MyMath2();
-		m2.n1 = 10;
-		m2.n2 = 20;
+//		m2.n1 = 10;
+//		m2.n2 = 20;
+		m2.plus();
+		m2.plus();
 		m2.plus();
 		m2.minus();
 		m2.multiple();
