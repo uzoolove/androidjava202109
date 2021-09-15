e719f1a0f64ccf1663f7d81e4abb677d58130d7d
KilyongJeong <uzoolove@gmail.com>
2021-09-15 10:09:04 +0900

    .

--- JavaLab/src/ch11/BellTest.java
+++ JavaLab/src/ch11/BellTest.java
@@ -2,18 +2,30 @@ package ch11;
 
 public class BellTest {
 	public static void main(String[] args) {
-		
+//		Bell b = new DoorBell();
+		Bell b = new AlarmBell();
+		b.ring();
 	}
 }
 
-class Bell{
-	
+abstract class Bell{
+	abstract void ring();
 }
 
 class DoorBell extends Bell{
-	
+	void ring() {
+		System.out.println("띵동~ 띵동~");
+	}
 }
 
 class AlarmBell extends Bell{
-	
+	void ring() {
+		System.out.println("띠리리리~ 띠리리리~");
+	}
 }
+
+
+
+
+
+
