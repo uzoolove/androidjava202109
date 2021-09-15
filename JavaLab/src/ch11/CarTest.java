b5212a051cbcf666c28e6ae3c76534cc3f3ecd63
KilyongJeong <uzoolove@gmail.com>
2021-09-15 10:46:18 +0900

    .

--- JavaLab/src/ch11/CarTest.java
+++ JavaLab/src/ch11/CarTest.java
@@ -2,7 +2,36 @@ package ch11;
 
 public class CarTest {
 	public static void main(String[] args) {
+		Car c1 = new Car();
+		Car c2 = new OpenCar();
+		Car c3 = new MyCar();
+		Car c4 = new MyCar2();
 		
+		cityTour(c1);
+		cityTour(c2);
+		cityTour(c3);
+		cityTour(c4);
+	}
+	static void cityTour(Car c) {
+		c.start();
+		c.drive();
+		
+		System.out.println("벽을 만났다.");
+		if(c instanceof Tank) {
+			((Tank)c).shoot();
+		}else {
+			System.out.println("벽을 돌아간다.");
+		}
+		
+		System.out.println("흙더미를 만났다.");
+		if(c instanceof Fork) {
+			((Fork)c).fork();
+		}else {
+			System.out.println("흙더미를 돌아간다.");
+		}
+		
+		c.stop();
+		System.out.println();
 	}
 }
 class Car{
@@ -38,13 +67,18 @@ interface Tank{
 interface Fork{
 	void fork();
 }
-
 class MyCar extends OpenCar implements Tank{
-	
+	public void shoot() {
+		System.out.println("포를 쏜다.");
+	}
 }
-
 class MyCar2 extends Car implements Tank, Fork{
-	
+	public void shoot() {
+		System.out.println("포를 쏜다.");
+	}
+	public void fork() {
+		System.out.println("흙더미를 치운다.");
+	}
 }
 
 
