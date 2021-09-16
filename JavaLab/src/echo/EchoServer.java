0c44740b65fc2d66510ed16ccdea8d6b477869cd
KilyongJeong <uzoolove@gmail.com>
2021-09-16 12:21:14 +0900

    .

--- JavaLab/src/echo/EchoServer.java
+++ JavaLab/src/echo/EchoServer.java
@@ -1,15 +1,17 @@
 package echo;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class EchoServer {
 	private ServerSocket ss;
 	private Socket s;
-	private InputStream in;
-	private OutputStream out;
+	private BufferedReader in;
+	private PrintWriter out;
 
 	private void startServer() {
 		try {
@@ -19,12 +21,14 @@ public class EchoServer {
 			// 클라이언트 접속 대기
 			s = ss.accept();
 			System.out.println("클라이언트 접속. " + s.getInetAddress().getHostAddress());
+
 			// 소켓에서 입출력 스트림 생성
-			in = s.getInputStream();
-			out = s.getOutputStream();
+			in = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));
+			out = new PrintWriter(new OutputStreamWriter(s.getOutputStream(), "UTF-8"));
 			// 입력 스트림에서 읽고 출력 스트림으로 전송한다.
 			int readData = 0;
 			while((readData = in.read()) != -1) {
+				System.out.write(readData);
 				out.write(readData);
 			}
 		} catch (Exception e) {
