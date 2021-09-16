c58895b87e24df9f9dd8508839220ce4aeff3392
KilyongJeong <uzoolove@gmail.com>
2021-09-16 11:55:09 +0900

    .

--- JavaLab/src/echo/EchoClient.java
+++ JavaLab/src/echo/EchoClient.java
@@ -1,6 +1,7 @@
 package echo;
 
 import java.net.Socket;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -28,13 +29,27 @@ public class EchoClient {
 		
 		try{
 			// 소켓 생성(서버 접속 요청)			
+			s = new Socket(host, port);
+			System.out.println("서버에 접속 완료.");
 			
 			// 소켓에서 입출력 스트림 생성
+			in = s.getInputStream();
+			out = s.getOutputStream();
 			
 			// 키보드에서 입력한 문자를 꺼내기 위한 입력 스트림 생성
+			key = System.in;
 			
 			// 키보드에서 입력한 데이터를 출력 스트림으로 전송
-			
+			int readData = 0;
+			while((readData = key.read()) != -1) {
+				out.write(readData);
+				
+				// 서버로부토 받은 데이터 출력
+				int recvData = 0;
+				recvData = in.read();
+				System.out.write(recvData);
+			}
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}
--- JavaLab/src/echo/EchoServer.java
+++ JavaLab/src/echo/EchoServer.java
@@ -6,47 +6,33 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class EchoServer {
-  private ServerSocket ss;
-  private Socket s;
-  private InputStream in;
-  private OutputStream out;
-	private void startServer(){
-		try{
+	private ServerSocket ss;
+	private Socket s;
+	private InputStream in;
+	private OutputStream out;
+
+	private void startServer() {
+		try {
 			// 서버소켓 생성
-			
+			ss = new ServerSocket(1234);
+			System.out.println("서버 구동 완료. 1234");
 			// 클라이언트 접속 대기
-			
+			s = ss.accept();
+			System.out.println("클라이언트 접속. " + s.getInetAddress().getHostAddress());
 			// 소켓에서 입출력 스트림 생성
-			
+			in = s.getInputStream();
+			out = s.getOutputStream();
 			// 입력 스트림에서 읽고 출력 스트림으로 전송한다.
-			
-		}catch(Exception e){
+			int readData = 0;
+			while((readData = in.read()) != -1) {
+				out.write(readData);
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		new EchoServer().startServer();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
