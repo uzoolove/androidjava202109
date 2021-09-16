b3ddb7f2e1c3460cfa3979deb9777187df83cc28
KilyongJeong <uzoolove@gmail.com>
2021-09-16 13:55:20 +0900

    .

--- JavaLab/src/echo/EchoClient.java
+++ JavaLab/src/echo/EchoClient.java
@@ -1,13 +1,10 @@
 package echo;
 
-import java.net.Socket;
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.Socket;
 
 public class EchoClient {
 	private Socket s;
