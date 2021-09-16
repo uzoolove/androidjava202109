77b3ffda1cf54c23d94658a616f98e9fd5909f67
KilyongJeong <uzoolove@gmail.com>
2021-09-16 10:11:05 +0900

    .

--- JavaLab/src/ch13/KeyboardToFile.java
+++ JavaLab/src/ch13/KeyboardToFile.java
@@ -1,5 +1,7 @@
 package ch13;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * 표준입력장치(키보드) -> 파일(output.txt)
@@ -8,7 +10,17 @@ package ch13;
  */
 public class KeyboardToFile {
 	public static void main(String[] args) {
-		
+		int readData = 0;
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream("output.txt");
+			while((readData = System.in.read()) != -1) {
+				fos.write(readData);
+			}
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
